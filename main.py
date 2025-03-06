97be1bff4f03b8b010de2a28ab3da36b033d08ab
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-03-06 12:10:15 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-# change it to print your name
+class Person_Adilet:
+    x = 5
 
-print("Hello, I am Piotr Brudny")
+print(Person_Adilet)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Adilet", 20)
+
+print(p1.name)
+print(p1.age)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("Adilet", 20)
+
+print(p1)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("Adilet", 20)
+p1.myfunc()
