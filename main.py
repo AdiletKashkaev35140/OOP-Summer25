5f338686f98e75e044c0bd5f6ff9bfb9c44afbd7
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-03-10 16:08:03 +0100

    Adilet Kashkaev 35140main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# change it to print your name
+class Student:
+    def __init__(self, first_name, last_name, index_number, nationality):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.index_number = index_number
+        self.nationality = nationality
 
-print("Hello, I am Piotr Brudny")
+    def display(self):
+        return f"Name: {self.first_name} {self.last_name}, Index Number: {self.index_number}, Nationality: {self.nationality}"
+
+students = [
+    Student("John", "Doe", "312345", "American"),
+    Student("Anna", "Smith", "567890", "British"),
+    Student("Ahmed", "Khan", "511223", "Pakistani")
+]
+
+for student in students:
+    print(student.display())
