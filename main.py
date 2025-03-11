64352338e234749ecf2b7a8ca95bc80583f880ab
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-03-11 10:54:45 +0100

    Adilet Kashkaev 35140 main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-# change it to print your name
-
-print("Hello, I am Piotr Brudny")
+my_age = 4  # integer
+pi_value = 3.14  # float
+greeting = "hello"  # string
+items_list = [10, "hello", 200.3]  # list
+person_info = {"mykey": "value", "name": "Adilet"}  # dictionary
+coordinates = (10, "hello", 200.3)  # tuple
+unique_letters = {"a", "b"}  # set
+is_happy = True  # boolean
