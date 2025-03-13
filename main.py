aef3206067dcc1b3eeb63af53d1247e93d89f96d
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-03-13 12:06:23 +0100

    Adilet Kashkaev 35140 main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# basic_data_structures.py
-
-# Example of a list
 my_list = [1, 2, 3, 4, 5]
 
-# Example of a dictionary
+
 my_dict = {'name': 'Alice', 'age': 25}
 
-# Example of a tuple
+
 my_tuple = (10, 20, 30)
 
-# Example of a set
+
 my_set = {1, 2, 3, 4}
 
-# Print the variables
 print("List:", my_list)
 print("Dictionary:", my_dict)
 print("Tuple:", my_tuple)
