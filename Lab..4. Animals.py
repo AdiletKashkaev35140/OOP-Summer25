fe4f50db1e98c0454de62ad1382e4365744bc8a2
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-03-20 12:19:02 +0100

    Create Lab..4. Animals.py

---
+++ Lab..4. Animals.py
@@ -0,0 +1,36 @@
+animals = [
+    {
+        "name": "Cat",
+        "group": "Mammals",
+        "number_of_legs": 4,
+        "skills": ["climbing", "jumping", "hunting"]
+    },
+    {
+        "name": "Eagle",
+        "group": "Birds",
+        "number_of_legs": 2,
+        "skills": ["flying", "hunting", "sharp vision"]
+    },
+    {
+        "name": "Frog",
+        "group": "Amphibians",
+        "number_of_legs": 4,
+        "skills": ["jumping", "swimming", "croaking"]
+    },
+    {
+        "name": "Shark",
+        "group": "Fish",
+        "number_of_legs": 0,
+        "skills": ["swimming", "hunting", "smelling blood"]
+    },
+    {
+        "name": "Snake",
+        "group": "Reptiles",
+        "number_of_legs": 0,
+        "skills": ["slithering", "camouflaging", "biting"]
+    }
+]
+
+
+for animal in animals:
+    print(animal)
