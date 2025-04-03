a3407f3149d3433a534de36449bae66233da232c
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-04-03 10:41:13 +0000

    Add new file

---
+++ w3_school/python_variables.py
@@ -0,0 +1,18 @@
+x = 21
+y = "Adilet"
+print(x)
+print(y)
+
+x = 35140
+y = "Adilet Kashkaev"
+print(type(x))
+print(type(y))
+
+x = "Adilet"
+#all the same 
+x = 'Adilet'
+
+a = 21
+A = "Adilet"
+print(a)
+print(A)
