114d605b85aa545729cbf47264ea89c2a231b18a
AdiletKashkaev35140 <adiletkashkaev03@gmail.com>
2025-04-03 11:18:02 +0000

    Add new file

--- w3_school/python_variables.py
+++ w3_school/python_variables.py
@@ -16,3 +16,20 @@ a = 21
 A = "Adilet"
 print(a)
 print(A)
+
+##variable names:
+myvar = "Adilet"
+my_var = "Adilet"
+_my_var = "Adilet"
+myVar = "Adilet"
+MYVAR = "Adilet"
+myvar2 = "Adilet"
+
+## values to multiple variables
+
+x, y, z, a = "study", "expereince", "work", "to_achieve_the_goal"
+
+print(x)
+print(y)
+print(z)
+print(a)
